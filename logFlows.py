f018f1642041e50ae14023905ec95e9090452907
sam <sjcomeau@wpi.edu>
2019-03-31 02:29:10 +0000

    the start of log flows

---
+++ logFlows.py
@@ -0,0 +1,30 @@
+# for usability
+import argparse
+import logging
+
+# for verification
+import os
+
+# for packet parsing
+import dpkt
+
+def parse_packet(packet):
+	return 0
+
+def parse_file(file):
+	for ts, pkt in dpkt.pcap.Reader(open(file, 'r')):
+		print ts, pkt
+
+	return 0
+
+def main():
+	parser = argparse.ArgumentParser(description="parse pcap files")
+	parser.add_argument("-f", "--file", required=True, help="the file to parse")
+	
+	args = parser.parse_args()
+
+	if not os.file.exists(args.file):
+		logging.error("input a valid file to be parsed")
+		exit()
+
+	parse_file(args.file)
