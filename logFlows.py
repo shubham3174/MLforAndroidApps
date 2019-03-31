57ca1492d328076b6b9e5b08836931d59042c4be
sam <sjcomeau@wpi.edu>
2019-03-31 03:15:04 +0000

    definint class for the packets

--- logFlows.py
+++ logFlows.py
@@ -8,8 +8,35 @@ import os
 # for packet parsing
 import pyshark
 
+# packet structure
+class Packet():
+	src_ip = None
+	dst_ip = None
+	src_port = None
+	dst_port = None
+	protocol = None
+	num_packets_sent = None
+	num_packets_recv = None
+	num_bytes_sent = None
+	num_bytes_recv = None
+	
+	def __init__(self, src_ip, src_port, dst_ip, dst_port, protocol):
+		self.src_ip = src_ip
+		self.src_port = src_port
+		self.dst_ip = dst_ip
+		self.dst_port = dst_port
+		self.protocol = protocol
+
+	def print(self):
+		print "Source IP: ", self.src_ip
+		print "Source Port: ", self.src_port
+		print "Destination IP: ", self.dst_ip
+		print "Destination Port: ", self.dst_port
+		print "Protocol: ", self.protocol
+
 def parse_packet(packet):
-	print packet.info
+	pkt = Packet(packet.src, packet.dst, None, None, None)
+	return pkt
 
 def parse_file(file):
 	packets = pyshark.FileCapture(file)
